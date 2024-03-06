f1bb2ba4189eeda8a8a39f786eb10befe8fab755
bablu <aniketpanchal07@gmail.com>
2024-03-06 22:49:34 +0530

    added Mongodb connection

--- server/dest/index.js
+++ server/dest/index.js
@@ -1,20 +1,19 @@
 "use strict";
-// const express = require("express");
-// const dotenv = require("dotenv");
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-// import router from "./Routes/Routes"
-const app = (0, express_1.default)();
 const dotenv_1 = __importDefault(require("dotenv"));
 const Routes_1 = __importDefault(require("./Routes/Routes"));
-dotenv_1.default.config();
+const connection_1 = __importDefault(require("./db/connection"));
+const app = (0, express_1.default)();
 app.use(express_1.default.json());
+dotenv_1.default.config({ path: "./.env" });
 const port = process.env.PORT || 2000; //5002
-// app.use(router,require("./Routes/Routes"))
 app.use(Routes_1.default);
+(0, connection_1.default)();
+// demo
 app.get("/", (req, res) => {
     res.send("Express + TypeScript Server");
 });
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,24 +1,19 @@
-// const express = require("express");
-// const dotenv = require("dotenv");
+
 
 import express, { Express, Request, Response, Router } from "express";
-// import router from "./Routes/Routes"
-const app: Express = express();
-
-
 import dotenv from "dotenv";
 import router from "./Routes/Routes";
-dotenv.config();
+import connectDb from "./db/connection";
 
+const app: Express = express();
 app.use(express.json());
-
+dotenv.config({path:"./.env"});
 const port = process.env.PORT || 2000; //5002
-
-
-// app.use(router,require("./Routes/Routes"))
 app.use(router);  
+connectDb();
 
 
+// demo
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
 });
