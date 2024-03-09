e157c28232e2263a57ada053c1384df0e86749bd
bablu <aniketpanchal07@gmail.com>
2024-03-09 18:59:36 +0530

    edited

--- server2/index.js
+++ server2/index.js
@@ -3,6 +3,8 @@ const express = require("express");
 const dotenv = require("dotenv");
 const router = require("./Routes/Routes");
 const connectDb = require("./db/connection");
+const cors = require("cors");
+
 
 
 
@@ -10,7 +12,7 @@ const connectDb = require("./db/connection");
 const app = express();
 app.use(express.json());
 dotenv.config({path:"./.env"});
-const port = process.env.PORT //5002
+const port = process.env.PORT //5003
 
 connectDb();
 
@@ -18,15 +20,19 @@ app.use(router);
 
 // demo
 app.get("/", (req, res) => {
-  res.send("Express + TypeScript Server");
+  res.send("Express + javascript ");
 });
 
 app.get("/hi", (req, res) => {
   res.send("hii expressss");
 });
 
+app.use(cors());
+
 app.listen(port, () => {
   console.log(
     `[server]: hello, my Server is running at http://localhost:${port}`
   );
 });
+
+module.exports = app;
