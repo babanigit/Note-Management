b56ec9928d4eb8af2e4a83a9c532580f59dc420b
bablu <aniketpanchal07@gmail.com>
2024-03-12 12:36:44 +0530

    added logout button

--- client/src/pages/1home/Home.tsx
+++ client/src/pages/1home/Home.tsx
@@ -1,10 +1,27 @@
 
 
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 const Home = () => {
+
+  function change() {
+    localStorage.removeItem("token")
+  }
   return (
-    <div>Home</div>
+    <>
+    <div className=' h-screen grid place-items-center place-content-center '>
+
+   
+    <div>hello</div>
+    <button 
+    className=' border-2 rounded-md border-black p-2 bg-red-100'
+    onClick={change} > <NavLink to={"/register"} >
+    logout
+      </NavLink> </button>
+   
+      </div>
+    </>
   )
 }
 
