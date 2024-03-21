a0e15d7690933c3d990aee1d29a21797641c1627
bablu <aniketpanchal07@gmail.com>
2024-03-21 23:51:44 +0530

    added some updates

--- frontend-note-app/src/App.tsx
+++ frontend-note-app/src/App.tsx
@@ -1,26 +1,102 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+
+import { useEffect, useState } from "react";
+
+import { Container } from "react-bootstrap";
+
+
+import styles from "./style/notePage.module.css";
+
+import { UserModel } from "./components/aModal/userModal";
+
+import * as NoteApi from "./components/aNetwork/note_api"
+
+// import LoginModel from "../../components/RegisterModel/LoginModel";
+// import RegisterModel from "../../components/RegisterModel/RegisterModel";
+// import NavBar from "../../components/navBar/NavBar";
+
+import NotesPageLoggedInView from "./components/bNotes/NotesPageLoggedInView"
+import NotesPageLogoutview from "./components/bNotes/NotesPageLogoutview";
+import RegModal from "./components/cRegLogModal/RegModal";
+import LogModal from "./components/cRegLogModal/LogModal";
+import NavBar from "./components/dNavBar/NavBar";
+
+
+const App = () => {
+
+  const [loggedInUser,setLoggedInUser] = useState<UserModel|null>(null);
+
+  // to show Registration modal
+  const[showRegModel,setShowRegModel]= useState(false);
+
+    // to show Login modal
+  const[showLogModel,setShowLogModel]=useState(false);
+
+
+  // getting the loggedInUser
+  useEffect(()=> {
+
+    async function fetchLoggedInUser() {
+      try {
+        const user= await NoteApi.getLoggedInUser();
+        setLoggedInUser(user)
+
+        console.log(user)
+        
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    fetchLoggedInUser();
+  },[])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+
+      <NavBar
+        loggedInUser={loggedInUser}
+        onLoginClicked={() =>setShowLogModel(true)}
+        onRegisterClicked={() => setShowRegModel(true)}
+        onLogoutSuccessful={() =>setLoggedInUser(null)}
+      />
+      <Container className={` bg-blue-100 ${styles.notesPage} `}>
+
+        {
+        loggedInUser 
+        // true
+        ?<NotesPageLoggedInView />
+      :(
+      <NotesPageLogoutview />
+      // <></>
+      )
+      }
+
+
+    
+      </Container>
+
+       {showRegModel &&
+          <RegModal
+            onDismiss={() => setShowRegModel(false)}
+            onRegistrationSuccessful={(user) => { 
+              setLoggedInUser(user);
+              setShowRegModel(false);
+            }}
+          />}
+
+        {showLogModel &&
+          <LogModal
+            onDismiss={() =>setShowLogModel(false)}
+            onLoginSuccessful={(user) => {
+              setLoggedInUser(user);
+              setShowLogModel(false);
+             }}
+          />} 
+
+
+    </>
   );
-}
+};
 
 export default App;
