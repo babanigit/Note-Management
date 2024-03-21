25ee84a5b776945a1a66326a772f0967553b1fea
bablu <aniketpanchal07@gmail.com>
2024-03-21 21:52:14 +0530

    remove gitignore files

--- client/src/util/FormateData.ts
+++
@@ -1,9 +0,0 @@
-export function FormatDate(dataString: string): string {
-    return new Date(dataString).toLocaleString("en-US", {
-        year: "numeric",
-        month: "short",
-        day: "numeric",
-        hour: "numeric",
-        minute: "numeric"
-    });
-}
