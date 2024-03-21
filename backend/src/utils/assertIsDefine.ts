862ae8d0bd334dc6375e755658c8d199c2944cea
bablu <aniketpanchal07@gmail.com>
2024-03-21 22:49:51 +0530

    deleted

--- backend/src/utils/assertIsDefine.ts
+++
@@ -1,9 +0,0 @@
-
-
-export function assertIsDefine<T>(val: T): asserts val is NonNullable<T> {
-
-    if (!val) {
-        console.log("error from assertisDEfine")
-        throw Error("Expected 'val' to be defined,but received ")
-    }
-}
